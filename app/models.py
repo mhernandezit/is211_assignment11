c532694729743068e661cc557bfde65fe33c5acc
mhernandezit <mhernandezit@gmail.com>
2019-04-11 10:26:04 -0400

    updating gitignore

--- app/models.py
+++ app/models.py
@@ -1,7 +1,4 @@
 from datetime import datetime
-import hashlib
-from werkzeug.security import generate_password_hash, check_password_hash
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app, request, url_for
 from . import db
 
--- todoapp.py
+++ todoapp.py
@@ -1,28 +1,9 @@
-from flask import Flask, render_template, session, redirect, url_for, flash
-from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from app import create_app
+import os
+import app.models
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
-
-bootstrap = Bootstrap(app)
-moment = Moment(app)
-
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
+application = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
